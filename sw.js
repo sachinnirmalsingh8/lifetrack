3139c56348bcbdf45154b81d12dfc54ad4b64bb9
sachin nirmal singh <sachingopal5@gmail.com>
2026-05-31 22:49:42 +0530

    Add backup/restore + PWA auto-update

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_VERSION = 'lifetrack-v2.1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_VERSION).then(cache => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
